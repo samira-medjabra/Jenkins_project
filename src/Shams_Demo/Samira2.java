e9393946893abb58e7b42ff6c4502969929d896b
HP <HP@DESKTOP-8I6N7R2.attlocal.net>
2024-04-22 19:21:57 -0700

    Class_28_Task01_concatenate and print, completed on 04-22-2024 Class_29&30_Task01_Calculate Average, completed on 04-22-2024 Class_31&32_task01_Printing using loops, completed on 04-22-2024

---
+++ src/Shams_Demo/Samira2.java
@@ -0,0 +1,37 @@
+package Shams_Demo;
+
+public class Samira2 {
+
+	public static void main(String[] args) {
+		// while break loop
+		
+		 int s=-5;
+	      while(s<=5) {
+	    	  System.out.println("s="+s);
+	      
+	    	  if(s==3) {
+	    		  System.out.println("samira");
+	    		  break;
+	    	  }
+	      }
+	      
+	      int j=-500;
+	      while(j<-200) {
+	    	  System.out.println("j="+j);
+	     if(j==-278) {
+	    	 System.out.println("samira");
+	    	 break;
+	     }
+	      }
+		
+		     
+	      
+	      
+		
+		
+		
+		
+
+	}
+
+}
--- src/Shams_Demo/TestClass.java
+++ src/Shams_Demo/TestClass.java
@@ -3,8 +3,116 @@ package Shams_Demo;
 public class TestClass {
 
 	public static void main(String[] args) {
-		// Hey Batch35 . welcome 
-         System.out.println("Hi everyone");
+		// Class_28_Task 01_Concatenate and print
+		int a=2;
+		int b=3;
+		int c=4;
+		float f=1.2f;
+		double l=1.3d;
+		String g="hello world";
+		
+		System.out.println(a+b+c);
+		System.out.println(a+g+b);
+		System.out.println(f+l+a);
+		System.out.println(g+f+a);
+		System.out.println(f+a+g);
+		System.out.println("f"+a+f+l);
+		System.out.println(g+a+l+g);
+		System.out.println(g+a+g+l);
+		System.out.println(a+b+g+(a+b+c));
+         
+		    //Class 29_30_Task_02_Calculate Average
+		int math=60;
+		int science=66;
+		int physiscs=48;
+		int biology= 74;
+		int total=math+science+physiscs+biology;
+		float average=total/4f;
+		System.out.println(average);
+		if (average>=90 && average<=100) {
+		System.out.println("grade of student=A");
+		}else if(average>=80 && average<=89) {
+			System.out.println("grade of student=B");
+		}else if (average>=70 && average<=79) {
+			System.out.println("grade of student= C");
+		}else if (average>=40 && average<=69) {
+			System.out.println("grade of student=D");
+		}else {
+			System.out.println("program is successful");
+		}
+		
+		
+		   // Class 31&32_Task_01_Loops
+		
+		int k=2;
+		while(k>-10) {
+			System.out.println("k="+k);
+			k--;
+		}
+		
+		
+		
+		int q=3;
+		while (q>=-10) {
+			System.out.println("q="+q);
+			q--;
+		}
+		
+		int w=3;
+		while(w<20) {
+			System.out.println("w="+w);
+			w++;
+		}
+		int e=-355;
+		while(e<-255) {
+			System.out.println("e="+e);
+			e++;
+		}
+		int r=-956;
+		while(r>-960) {
+			System.out.println("r="+r);
+			r--;
+		}
+		
+		int t=-10;
+		while(t<10) {
+			System.out.println("t="+t);
+			t++;
+		}
+		                   //Do loop
+		
+	      int y=2;
+	      do {
+	    	  System.out.println("y="+y);
+	    	  y++;
+	      }
+	      while(y<-10);
+		
+		
+	      int u=3;
+	      do {
+	    	  System.out.println("u="+u);
+	    	  u++;
+	      }
+	      while(u<20);
+	      
+	                     // For loop
+	      for(int i=2; i>-10; i--) {
+	      System.out.println("i"+i);
+	      }
+	      for(int p=3; p<20; p++) {
+	    	  System.out.println("p="+p);
+	      }
+	      
+	     
+	      
+	      
+	      
+	      
+	      
+		
+		
+		
 	}
 
 }
