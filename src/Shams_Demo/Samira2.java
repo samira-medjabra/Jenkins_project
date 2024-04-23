ecf6654f42f4ba15db775ee41a3e06af1e89aec8
HP <HP@DESKTOP-8I6N7R2.attlocal.net>
2024-04-22 19:24:47 -0700

    Class 31&32_break, completed on 04-22-2024

--- src/Shams_Demo/Samira2.java
+++ src/Shams_Demo/Samira2.java
@@ -3,7 +3,7 @@ package Shams_Demo;
 public class Samira2 {
 
 	public static void main(String[] args) {
-		// while break loop
+		// Class 31&32_while break loop
 		
 		 int s=-5;
 	      while(s<=5) {
